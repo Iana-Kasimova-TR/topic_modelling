d7b872998da5a1c9c2a3c2b0576c2a525c96b8f4
Iana Kasimova <Iana_Kasimova@epam.com>
2022-08-09 16:32:09 +0200

    fix commit notices

--- topic_model.py
+++ topic_model.py
@@ -1,30 +1,21 @@
-import json
+"""module for keeping logic for topic modelling"""
 import re
 import string
 from collections import Counter
-from typing import Dict, List
 
-import nltk
 import numpy as np
 import pandas as pd
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from numpy import linalg as LA
 from scipy.sparse import csr_matrix
-from sklearn import neighbors
-from sklearn.decomposition import TruncatedSVD
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.preprocessing import normalize
 from sklearn.utils.extmath import randomized_svd
-
-# fit(return state) and predict(use twice) separately
-# sent path to file and iterate, methods accept generators
-# for demo not needed all dataset (slice), polars(replacement for pandas)
 
 
 def fit(path_to_file: str, name_text_column: str):
-    token_number = {}
-    token_quantity = {}
+    """method which provide document-term matrix"""
+    token_number: dict = {}
+    token_quantity: dict = {}
     counters_docs = []
     for line in pd.read_csv(path_to_file, chunksize=1, sep=","):
         line = clean_text(line[name_text_column])
@@ -33,28 +24,31 @@ def fit(path_to_file: str, name_text_column: str):
         )
         counters_docs.append(Counter(line))
     document_term_mtx = get_document_term_matrix(counters_docs, token_number)
-    U, _, _ = randomized_svd(
+    matrix, _, _ = randomized_svd(
         document_term_mtx, n_components=5, n_iter=5, random_state=None
     )
-    return U
+    return matrix
 
 
-def predict(U, index: int, number_neighbors: int):
-    return get_neighbors(index, U, number_neighbors)
+def predict(matrix, index: int, number_neighbors: int):
+    """method which will return indexes the closest documents"""
+    return get_neighbors(index, matrix, number_neighbors)
 
 
 def get_base_for_matrix(counter_docs, token_number):
+    """nested method"""
     ar_row = []
     ar_col = []
     ar_val = []
     for idx, item in enumerate(counter_docs):
         ar_row.extend(np.repeat(idx, len(item)))
         ar_col.extend([token_number.get(token) for token in item.keys()])
-        ar_val.extend([value for value in item.values()])
+        ar_val.extend(list(item.values()))
     return ar_row, ar_col, ar_val
 
 
 def get_token_dictionaries(line, token_number, token_quantity):
+    """nested method"""
     counter = 0
     for word_item in line:
         if word_item not in token_number.keys():
@@ -64,17 +58,19 @@ def get_token_dictionaries(line, token_number, token_quantity):
     return token_number, token_quantity
 
 
-# use cos distance, consider 10 docs(query) 1*256 - doc on 256*90 - docs, sort, avrage precision at key.
-# each row in matrix divide on ||x|| = sqrt(sum(x_i^2)), where x it is row
-def get_neighbors(idx, U, k):
-    norms = LA.norm(U, axis=1, keepdims=True)
-    U_norm /= norms
-    doc = U_norm[idx]
-    mult = np.matmul(U_norm, doc)
-    return np.argpartition(a=mult, kth=-k, axis=1)[-k:]
+def get_neighbors(idx, matrix, number_of_neighbors):
+    """nested method"""
+    norms = LA.norm(matrix, axis=1, keepdims=True)
+    matrix_norm = matrix / norms
+    doc = matrix_norm[idx]
+    mult = np.matmul(matrix_norm, doc)
+    return np.argpartition(a=mult, kth=-number_of_neighbors, axis=1)[
+        -number_of_neighbors:
+    ]
 
 
 def get_document_term_matrix(counter_docs, token_number):
+    """nested method"""
     ar_row, ar_col, ar_val = get_base_for_matrix(counter_docs, token_number)
     sparse_mtx = csr_matrix(
         (ar_val, (ar_row, ar_col)), shape=(len(counter_docs), len(token_number))
@@ -83,10 +79,12 @@ def get_document_term_matrix(counter_docs, token_number):
 
 
 def get_tokens_from_doc(doc):
+    """nested method"""
     return set(doc)
 
 
 def count_quantity_token_in_doc(doc, token):
+    """nested method"""
     counter = 0
     for item in doc:
         if item == token:
@@ -94,60 +92,52 @@ def count_quantity_token_in_doc(doc, token):
     return counter
 
 
-def get_tf_idf_matrix(mx):
-    number_of_words_in_doc = np.matrix(mx.getnnz(axis=1)).transpose()
-    tf = mx.multiply(1 / number_of_words_in_doc)
-    number_docs = mx.shape[0]
-    item = mx.getnnz(axis=0)
-    df = tf.multiply(np.log(number_docs / item))
-    return tf.multiply(df)
+def get_tf_idf_matrix(matrix):
+    """nested method"""
+    number_of_words_in_doc = np.matrix(matrix.getnnz(axis=1)).transpose()
+    tf_part = matrix.multiply(1 / number_of_words_in_doc)
+    number_docs = matrix.shape[0]
+    item = matrix.getnnz(axis=0)
+    dataframe = tf_part.multiply(np.log(number_docs / item))
+    return tf_part.multiply(dataframe)
 
 
 def clean_text(corpus: pd.Series):
+    """nested method"""
     porter_stemmer = PorterStemmer()
-    corpus = corpus.apply(lambda x: remove_punctuation(x))
-    corpus = corpus.apply(lambda x: tokenization(x))
-    corpus = corpus.apply(lambda x: remove_stopwords(x))
+    corpus = corpus.apply(remove_punctuation)
+    corpus = corpus.apply(tokenization)
+    corpus = corpus.apply(remove_stopwords)
     corpus = corpus.apply(lambda x: [item.lower() for item in x])
     corpus = corpus.apply(lambda x: [porter_stemmer.stem(word) for word in x])
     return corpus.to_numpy()[0]
 
 
 def remove_punctuation(text):
+    """nested method"""
     punctuation_free = "".join([i for i in text if i not in string.punctuation])
     return punctuation_free
 
 
 def tokenization(text):
+    """nested method"""
     tokens = re.split(r"\W+", text)
     return tokens
 
 
 def remove_stopwords(text):
+    """nested method"""
     output = [i for i in text if i not in stopwords.words("english")]
     return output
 
 
-# use cos distance, consider 10 docs(query) 1*256 - doc on 256*90 - docs, sort, avrage precision at key.
-# each row in matrix divide on ||x|| = sqrt(sum(x_i^2)), where x it is row
-def get_neighbors(idx, u, k):
-    doc = u[idx]
-    docs = np.vstack(
-        (
-            u[:idx, :],
-            u[
-                idx + 1 :,
-            ],
-        )
-    )
-    mult = np.matmul(docs, doc)
-    return np.argsort(mult)[-k:]
-
-
-def get_correctness_doc_count(df, index_doc, u, k):
-    neighbors = get_neighbors(index_doc, u, k)
+def get_correctness_doc_count(
+    dataframe, index_doc, matrix, number_of_neighbors, name_of_target
+):
+    """method wich will return value of metric"""
+    neighbors = get_neighbors(index_doc, matrix, number_of_neighbors)
     counter = 0
     for item in neighbors:
-        if df.Category[item] == df.Category[index_doc]:
+        if dataframe[name_of_target][item] == dataframe[name_of_target][index_doc]:
             counter += 1
-    return counter / k
+    return counter / number_of_neighbors
